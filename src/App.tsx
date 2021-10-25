9232b1d47f2facc7a92c385934af113b9f37abb2
Leonardo Alves <leoves.oliveira@gmail.com>
2021-10-25 20:51:32 -0300

    add title

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 export const App = () => {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <h1>Leonardo Alves</h1>
       </header>
     </div>
   )
