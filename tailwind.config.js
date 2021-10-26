785e0b17079117f3d211c38315dfae4733c63d2b
Leonardo Alves <leoves.oliveira@gmail.com>
2021-10-25 21:03:46 -0300

    add styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./public/**/*.html', './src/**/*.{ts,tsx}'],
+  purge: ['./**/*.html', './src/**/*.{ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
