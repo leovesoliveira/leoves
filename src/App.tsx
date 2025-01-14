29c2bb8ec2fa2d16fb2ce025af34e3fa77595b29
Leonardo Alves <leoves.oliveira@gmail.com>
2025-01-13 20:13:35 -0500

    remove old files

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const App = () => {
-  return (
-    <div className="h-screen flex justify-center items-center bg-gray-900">
-      <h1 className="text-white font-bold text-4xl">Leonardo Alves</h1>
-    </div>
-  )
-}
