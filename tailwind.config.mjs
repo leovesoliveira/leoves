fd55f2db62aff6ed31cac21a6740fd77ff5bab3e
Leonardo Alves <leoves.oliveira@gmail.com>
2025-01-15 15:57:00 -0500

    add necessary libs and configs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 export default {
+	darkMode: 'selector',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+        'sans': ['"Montserrat"', ...defaultTheme.fontFamily.sans],
+				'handwritten': ['"Playwrite AU SA"', 'serif'],
+      },
+		},
 	},
 	plugins: [],
 }
