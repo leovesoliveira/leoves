dbf9dea188128314a3d016461cea9785dbb2bee2
Leonardo Alves <leoves.oliveira@gmail.com>
2025-01-15 15:58:53 -0500

    add interface title

---
+++ src/components/ui/LeonardoAlves.tsx
@@ -0,0 +1,12 @@
+export default function LeonardoAlves() {
+  return (
+    <div>
+      <span className="mb-2 ml-2 block text-xs text-slate-600 sm:mb-0 sm:ml-3 sm:text-base dark:text-slate-300">
+        <i>Hello, World! My name is</i>
+      </span>
+      <h1 className="font-handwritten text-2xl font-bold leading-none text-teal-600 transition duration-700 ease-in-out hover:-rotate-2 hover:scale-110 sm:text-4xl">
+        Leonardo Alves
+      </h1>
+    </div>
+  );
+}
